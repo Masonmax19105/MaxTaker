7f34599871e2c8c080da70876c41c22c0351008f
Masonmax19105 <102488515+Masonmax19105@users.noreply.github.com>
2022-05-02 10:49:24 -0500

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,44 @@
+
+ /$$      /$$                  /$$$$$$$$        /$$                          
+| $$$    /$$$                 |__  $$__/       | $$                          
+| $$$$  /$$$$  /$$$$$$  /$$   /$$| $$  /$$$$$$ | $$   /$$  /$$$$$$   /$$$$$$            MaxTaker
+| $$ $$/$$ $$ |____  $$|  $$ /$$/| $$ |____  $$| $$  /$$/ /$$__  $$ /$$__  $$           Created by Dasherr#4878  
+| $$  $$$| $$  /$$$$$$$ \  $$$$/ | $$  /$$$$$$$| $$$$$$/ | $$$$$$$$| $$  \__/           Edited by Masonmax1#1958
+| $$\  $ | $$ /$$__  $$  >$$  $$ | $$ /$$__  $$| $$_  $$ | $$_____/| $$      
+| $$ \/  | $$|  $$$$$$$ /$$/\  $$| $$|  $$$$$$$| $$ \  $$|  $$$$$$$| $$      
+|__/     |__/ \_______/|__/  \__/|__/ \_______/|__/  \__/ \_______/|__/      
+                                                                             
+                                                                             
+                                                                             
+
+                                                                                                                                                                                                                                                                                                                                                                                                           
+
+1. Pinger                                    16. Instagram ID Searcher
+
+2. Ip Lookup                                 17. Hazard Nuker
+ 
+3. Port Scanner                              18. XS-Nitro
+ 
+4. Open Putty                                19. EXIT
+
+5. 30+ Phishing Sites                        
+                             
+6. Keyword Generator Tool                    
+                             
+7. Vpn/Ovh                                  
+   
+8. Open Cyberhub                             
+    
+9. Open Vedbex                              
+    
+10. Ascii Art                                
+              
+11. Random Ip Generator                      
+ 
+12. Steam Account Generator                  
+
+13. Minecraft & Spotify Generator            
+
+14. Sick Fonts (Text etc.)                   
+
+15. Botting Panel (All Socials)                            
